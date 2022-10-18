915d4f35e0cb938b35c45c4be5674f73afd5c1e8
Usuario <Usuario@CPD02-M05>
2022-10-18 12:21:20 -0300

    Adicionar arquivos de projeto.

---
+++ AppMotivacional/AppMotivacional/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("School To Learn.ttf", Alias = "fonte1")]
+[assembly: ExportFont("Valentine Cute.ttf", Alias = "fonte2")]
+
+namespace AppMotivacional
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
